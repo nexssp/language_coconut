de8d677f6ae006cf673fb7c662828d93d8d88443
Marcin Polak <mapoart@gmail.com>
2020-10-04 23:42:04 +0200

    Linux adjustments

--- coconut.linux.nexss.config.js
+++ coconut.linux.nexss.config.js
@@ -1,14 +1,14 @@
 let languageConfig = Object.assign({}, require("./coconut.win32.nexss.config"));
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
+const sudo = os.sudo();
 languageConfig.compilers = {
   coconut: {
-    install: `apt install -y gcc python3.8 python3-pip && pip3 install coconut`,
+    install: `${sudo}apt install -y gcc python3.8 python3-pip && ${sudo}pip3 install coconut`,
     command: "coconut-run",
     args: "<file>",
     help: ``,
   },
 };
-
-const os = require(`@nexssp/os`);
 
 const distName = os.name();
 languageConfig.dist = distName;
@@ -19,19 +19,25 @@ switch (distName) {
   case os.distros.RHEL:
   case os.distros.FEDORA:
     languageConfig.compilers.coconut.install = os.replacePMByDistro(
-      "apt install -y python3-devel && " +
+      `${sudo}apt install -y python3-devel && ` +
         languageConfig.compilers.coconut.install
     );
     break;
   case os.distros.ORACLE:
     languageConfig.compilers.coconut.install = os.replacePMByDistro(
-      "apt install -y oracle-epel-release-el7 *python3-dev* && " +
+      `${sudo}apt install -y oracle-epel-release-el7 *python3-dev* && ` +
+        languageConfig.compilers.coconut.install
+    );
+    break;
+  case os.distros.UBUNTU:
+    languageConfig.compilers.coconut.install = os.replacePMByDistro(
+      `${sudo}apt install -y python3-dev && ` +
         languageConfig.compilers.coconut.install
     );
     break;
   default:
     languageConfig.compilers.coconut.install = os.replacePMByDistro(
-      "apt install -y *python3-dev* && " +
+      `${sudo}apt install -y *python3-dev* && ` +
         languageConfig.compilers.coconut.install
     );
     break;
